49f8281a05ed106e5072da06835a409e9770cc63
Pavel <js.webzap@gmail.com>
2023-02-16 17:14:21 +0300

    Ready cycles-1-tusk

---
+++ script_cycles.js
@@ -0,0 +1,130 @@
+"use strict";
+
+// let num = 50;
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// // do {
+// //     console.log(num);
+// //     num++;
+// // }
+// // while (num < 55);
+
+
+// // for (let num = 1; 1 < 8; num++){
+// //     if (num === 6) {
+// //         // break;
+// //         continue;
+// //     }
+// //     console.log(num);
+// // }
+
+// // for (let number = 5; 5 < 11; number++){
+// //     if (number === 11){
+// //         break;
+// //     }
+// //     console.log(number);
+// // }
+
+// // for (let number = 20; 20 > 9; number--){
+// //     if (number === 9){
+// //         break;
+// //     } else if (number === 12){
+// //         break;
+// //     }
+// //     console.log(number)
+// // }
+
+// // for (let number = 2; )
+
+// // for (let i = 2; i <= 11; i++){
+// //     if (i % 2 != 0) {
+// //         continue;
+// //     } else {
+// //         console.log(i);
+// //     }
+// // }
+
+// // let i = 0;
+// // while (i <= 10) {
+// //     i++;
+// //     if (i % 2 != 0) {
+// //         continue;
+// //     } else{
+// //         console.log(i);
+// //     }
+// // }
+
+// let num = 0;
+// while (num <= 50) {
+
+//     if (num === 4){
+//         break;
+//     } else {
+//         console.log(num);
+//     }
+// }
+
+// let num = 50;
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+function firstTask() {
+    let i = 5;
+    while ( i <= 10) {
+        console.log(i);
+        i++;
+    }
+}
+
+function secondTask() {
+    for (let i = 20; i >= 10; i--){
+        console.log(i);
+    }
+}
+
+function thirdTask() {
+    for (let i = 2; i <= 10; i++){
+        if ( i % 2 != 0){
+            continue;
+        }
+        console.log(i);
+    }
+}
+
+function fourthTask() {
+    let i = 0;
+    while ( i <= 10) {
+        i++;
+        if ( i % 2 != 0){
+            continue;
+        }
+        console.log(i);
+    } 
+}
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+    for (i = 5; i <= 10; i++){
+        arrayOfNumbers.push(i);
+    }
+    console.log(arrayOfNumbers);
+    // for (let i = 0; i < arrayOfNumbers.length; i++){
+    //     console.log(arrayOfNumbers[i]);
+    // }
+}
+
+return arrayOfNumbers;
+// Заполните массив цифрами от 5 до 10 включительно. Помните, что элементы массива можно сформировать так же, как и обращаться к ним: arr[0]
+
+// Ответ с кодом этих задач можно найти тут: ссылка
+
+// Если у вас получилось немного по другому, но решение засчитывается - то все в порядке. Помните, что вариантов решения всегда несколько. Например, <11 и <=10 в условиях цикла дадут одинаковый результат.
+
+// Но постарайтесь решить самостоятельно 🙂
+
+
